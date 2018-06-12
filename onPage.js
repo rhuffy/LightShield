e29aa8c3f097ee1c3d17d2c68773cfc989c5624d
rhuffy <rhuffy@me.com>
2018-06-12 13:50:18 -0400

    hover over transparent gifs to reveal them after a delay

--- onPage.js
+++ onPage.js
@@ -1,24 +1,37 @@
-var picture = chrome.extension.getURL("images/pepe.jpg"),
-    video = chrome.extension.getURL("images/avo.mp4"),
-    images = document.getElementsByTagName("img"),
+var images = document.getElementsByTagName("img"),
     videos = document.getElementsByTagName("video");
 
+var minOpacity = 0.1;
 
-
-setInterval(replaceElements, 500);
+replaceElements();
+setInterval(replaceElements, 100);
 
 
 function replaceElements(){
   for (var i = 0; i < images.length; i++) {
     var len = images[i].src.length;
     if (images[i].src.substring(len - 3, len) === "gif" || images[i].alt.substring(len - 3, len) === "gif" || images[i].src.includes(".gif") || images[i].alt.includes("gif")){
-      images[i].setAttribute("style", "opacity:0.1")
-      //images[i].setAttribute("src", picture);
-      //images[i].setAttribute("srcset", picture);
+      if (!images[i].hasAttribute('inside')){
+        images[i].setAttribute('inside', 0);
+      }
+      document.images[i].onload = obscure;
+      document.images[i].onmouseenter = reveal;
+      document.images[i].onmouseleave = obscure;
     }
   }
-  // for (var i = 0; i < videos.length; i++) {
-  //   var len = videos[i].src.length;
-  //   videos[i].setAttribute("src", video);
-  // };
+}
+
+function reveal(){
+  var thing = this;
+  this.setAttribute('inside', 1);
+  setTimeout(function() {
+    if (thing.getAttribute('inside') == 1){
+      thing.style.opacity = 1;
+    }
+  }, 2000);
+}
+
+function obscure(){
+  this.setAttribute('inside', 0);
+  this.style.opacity = minOpacity;
 }
